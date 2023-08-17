1f9f896c111811e334eba9dda33d1a41406350a5
Ricardo Osorio <ricardojesusosorio@gmail.com>
2023-08-17 10:34:27 -0400

    added key to list items

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -6,7 +6,9 @@ function ListGroup() {
       <h1>List</h1>
       <ul className="list-group">
         {items.map((item) => (
-          <li className="list-group-item">{item}</li>
+          <li key={item} className="list-group-item">
+            {item}
+          </li>
         ))}
       </ul>
     </>
