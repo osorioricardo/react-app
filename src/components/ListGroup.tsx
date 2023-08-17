d8f0affea02489dc450196ab9df05ce485b35fad
Ricardo Osorio <ricardojesusosorio@gmail.com>
2023-08-17 10:46:43 -0400

    added short hand conditional consicely

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -6,7 +6,7 @@ function ListGroup() {
   return (
     <>
       <h1>List</h1>
-      {items.length === 0 ? <p>No item found</p> : null}
+      {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item} className="list-group-item">
