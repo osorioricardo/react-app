1affa874abc71eac582f12d4a54ec2165fdf510d
Ricardo Osorio <ricardojesusosorio@gmail.com>
2023-08-17 10:38:20 -0400

    removed redundancy and added shorthand conditional

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -3,17 +3,10 @@ function ListGroup() {
 
   items = [];
 
-  if (items.length === 0)
-    return (
-      <>
-        <h1>List</h1>
-        <p>No item found</p>
-      </>
-    );
-
   return (
     <>
       <h1>List</h1>
+      {items.length === 0 ? <p>No item found</p> : null}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item} className="list-group-item">
